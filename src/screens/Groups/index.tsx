4d85b1c23966ca6d8fb5ce4e5258a524cb5924c7
Flavio Martil <flaviomartil5@gmail.com>
2024-10-18 02:11:40 -0300

    Module 2 - Using a guide to have a theme for all application, make easier to maintenance, and removed unused types from ts/babel

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,10 @@
 import {
     Container,
-    Title
 } from "@screens/Groups/styles";
 
 export function Groups() {
     return (
         <Container>
-          <Title>Groups</Title>
         </Container>
     );
 }
