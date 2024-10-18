bcef31aae17ee7b02f0d739f4a5ed809a670d6c3
Flavio Martil <flaviomartil5@gmail.com>
2024-10-18 01:46:24 -0300

    Module 2 - Styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    Container,
+    Title
+} from "@screens/Groups/styles";
 
 export function Groups() {
     return (
-        <View style={styles.container}>
-            <Text>Groups</Text>
-            <StatusBar style="auto" />
-        </View>
+        <Container>
+          <Title>Groups</Title>
+        </Container>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
