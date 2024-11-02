1d2353b06bfb9b7cc518ececfdaf5cfdd516f08a
Flavio Martil <flaviomartil5@gmail.com>
2024-11-02 15:24:36 -0300

    Screen Players, and configs for webstorm prettier.xml / eslint and assets not commited before.

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,10 +7,20 @@ import { Filter } from '@components/Filter';
 import { FlatList } from 'react-native';
 import { useState } from 'react';
 import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Rodrigo', 'Vini']);
+  const [players, setPlayers] = useState([
+    'Rodrigo',
+    'Vini',
+    'Pedro',
+    'vicente',
+    'teste',
+    'teste2',
+    'teste3',
+  ]);
 
   return (
     <Container>
@@ -50,7 +60,17 @@ export function Players() {
             onRemove={() => {}}
           />
         )}
+        ListEmptyComponent={<ListEmpty message="Não há pessoas nesse time" />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          ...(players.length === 0 ? [{ flex: 1 }] : []),
+        ]}
       />
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      ></Button>
     </Container>
   );
 }
